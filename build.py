c5292d7fb79fa0d81c3d0c11b8348662fb4eab0c
Shaan <s.hossain@student.tudelft.nl>
2021-05-02 23:51:51 +0200

    modified build

--- build.py
+++ build.py
@@ -6,12 +6,15 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-
+use_plugin("python.pycharm")
+use_plugin("python.install_dependencies")
 
 name = "interactive-data-visualization"
-default_task = "publish"
+default_task = ["clean", "analyze", "publish"]
 
 
 @init
 def set_properties(project):
     project.set_property("dir_docs", "doc")
+    project.set_property("flake8_break_build", False)
+    project.set_property("coverage_break_build", False)
