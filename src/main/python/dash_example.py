bdb67f568d486efa7c90d11d0f54b884ea25a286
Shaan <s.hossain@student.tudelft.nl>
2021-05-10 16:00:21 +0200

    scatterplot remove lines

--- src/main/python/dash_example.py
+++ src/main/python/dash_example.py
@@ -168,7 +168,7 @@ def update_graph(contents, filename, xvalue, yvalue):
                 go.Scatter(
                     x=x,
                     y=y,
-                    mode='lines+markers')
+                    mode='markers')
                 ],
             layout=go.Layout(
                 plot_bgcolor=colors["graphBackground"],
