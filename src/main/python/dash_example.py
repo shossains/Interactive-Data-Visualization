ce0a5c594b2a1b85abc031a609f687ff7df6d10d
joostjansenjan <j.j.jansen-2@student.tudelft.nl>
2021-05-11 14:41:20 +0200

    Added density function and checkboxes to turn scatter and density label on and off

--- src/main/python/dash_example.py
+++ src/main/python/dash_example.py
@@ -1,6 +1,7 @@
 import base64
 import io
 import plotly.graph_objs as go
+import plotly.express as px
 import time
 
 import dash
@@ -10,13 +11,10 @@ import dash_html_components as html
 import dash_table
 import pandas as pd
 
-df = pd.DataFrame({})
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-
 
 colors = {
     "graphBackground": "#F5F5F5",
@@ -46,56 +44,50 @@ app.layout = html.Div([
     ),
     html.H4("Select machine learning tool"),
     dcc.Dropdown(
-                id='select-tool',
-                options=[
-                    {'label': 'Choose ML method', 'value': 'index'},
-                    {'label': 'T-sne (not implemented)', 'value': 'T-sne'},
-                    {'label': 'other_ml_tool  (not implemented)', 'value': 'other_ml_tool'}
-                ],
-                value='index'
-            ),
-    # When T-sne choosen this one will be visible
+        id='select-tool',
+        options=[
+            {'label': 'Choose ML method', 'value': 'index'},
+            {'label': 'T-sne (not implemented)', 'value': 'T-sne'},
+            {'label': 'other_ml_tool  (not implemented)', 'value': 'other_ml_tool'}
+        ],
+        value='index'
+    ),
+    # When T-sne chosen this one will be visible
     html.Div([
-    html.H4("Select variable x"),
-    dcc.Dropdown(
-        id='select-variable-x',
-        placeholder = 'Select ...'),
-    html.H4("Select variable y"),
-    dcc.Dropdown(
-        id='select-variable-y',
-        placeholder='Select ...'),
-    html.H4("Select characteristic to display on hover"),
-    dcc.Dropdown(
-        id='select-characteristics',
-        placeholder='Select ...'),
-    html.Div(id='output-select-data'),
-    dcc.Graph(id='Mygraph'),
-    html.Div(id='output-data-upload')],
-    id='t-sne', style={'display': 'block'}),
+        html.H4("Select variable x"),
+        dcc.Dropdown(
+            id='select-variable-x',
+            placeholder='Select ...'),
+        html.H4("Select variable y"),
+        dcc.Dropdown(
+            id='select-variable-y',
+            placeholder='Select ...'),
+        html.H4("Select Characteristics"),
+        dcc.Dropdown(
+            id='select-characteristics',
+            placeholder='Select ...',
+            # multi=True
+        ),
+        dcc.Checklist(id='select-plot-options',
+                      options=[
+                          {'label': 'Scatter', 'value': 'scatter'},
+                          {'label': 'Density', 'value': 'density'},
+                      ],
+                      value=['scatter']
+                      ),
+        html.Div(id='output-select-data'),
+        dcc.Graph(id='Mygraph'),
+        html.Div(id='output-data-upload')],
+        id='t-sne', style={'display': 'block'}),
 ])
 
-@app.callback(Output('dummy', 'children'),
-                [
-                    Input('upload-data', 'contents'),
-                    Input('upload-data', 'filename')
-                ])
-def update_dataframe(contents, filename):
-    print("called")
-    if contents:
-        contents = contents[0]
-        filename = filename[0]
-        global df
-        df = parse_data(contents, filename)
-        df = df.reset_index()
-        print(len(df))
 
 @app.callback(
     Output(component_id='t-sne', component_property='style'),
     [Input(component_id='select-tool', component_property='value')])
-
-def show_hide_t_sne(visibility_state):
+def show_hide_element(visibility_state):
     """
-    Looks at wich tool is selected in the dropdown select-tool and displays selection functions for that certain tool.
+    Looks at which tool is selected in the dropdown select-tool and displays selection functions for that certain tool.
     TODO: Add more return states for more tools.
     Joost knows this^
     :param visibility_state:
@@ -108,74 +100,63 @@ def show_hide_t_sne(visibility_state):
     if visibility_state == 'index':
         return {'display': 'none'}
 
-@app.callback(
-    Output(component_id='output-data-upload', component_property='style'),
-    [Input(component_id='select-table', component_property='value')])
-
-def show_hide_table_scatter(visibility_state):
-    """
-    Hides or shows the data in a table at the bottom
-    :param visibility_state: Selected or unselected
-    :return: style format visible or not
-    """
-    print(visibility_state)
-    if visibility_state == 'raw-data':
-        return {'display': 'block'}
-    if visibility_state == []:
-        return {'display': 'none'}
 
 @app.callback([Output('select-variable-x', 'options'),
-               Output('select-variable-y', 'options')],
-            [
-                Input('dummy', 'children')
-            ])
-def set_options_variable(dummy):
-    print("THIS IS cALLed")
+               Output('select-variable-y', 'options'),
+               Output('select-characteristics', 'options')],
+              [
+                  Input('upload-data', 'contents'),
+                  Input('upload-data', 'filename'),
+              ])
+def set_options_variable(contents, filename):
     """
     loads in possible parameters for the x and y-axis from the data.
     TODO: Load the data in ones as a global variable. At the moment df is loaded in per function (inefficient).
     :param contents: contents of the data
     :param filename: filename of the data
-    :return: Possible options for dropdown x-axis.
+    :return: Possible options for dropdown x-axis, y-axis and characteristic.
     """
-    # df = pd.DataFrame({})
-    # if (contents is not None):
-    #     if contents:
-    #         contents = contents[0]
-    #         filename = filename[0]
-    #         global df
-    #         df = df.reset_index()
-    # return [{'label': i, 'value': i} for i in df.columns], [{'label': i, 'value': i} for i in df.columns]
-
-    if dummy:
-        global df
-        # df = df.reset_index()
-    return [{'label': i, 'value': i} for i in df.columns], [{'label': i, 'value': i} for i in df.columns]
+    df = pd.DataFrame({})
+    if contents is not None:
+        if contents:
+            contents = contents[0]
+            filename = filename[0]
+            df = parse_data(contents, filename)
+            df = df.reset_index()
+    return [{'label': i, 'value': i} for i in df.columns], [{'label': i, 'value': i} for i in df.columns], [
+        {'label': i, 'value': i} for i in df.columns]
 
 
 @app.callback([Output('select-variable-x', 'value'),
-               Output('select-variable-y', 'value')],
-            [
-                Input('select-variable-x', 'options'),
-                Input('select-variable-y', 'options')
-            ])
-def set_variables(options_x, options_y):
+               Output('select-variable-y', 'value'),
+               Output('select-characteristics', 'value')],
+              [
+                  Input('select-variable-x', 'options'),
+                  Input('select-variable-y', 'options'),
+                  Input('select-characteristics', 'options')
+              ])
+def set_variables(options_x, options_y, options_char):
     """
     Gets the ouput of the dropdown of the 'select-variable-x' and 'select-variable-y'.
     :param options_x: All possible x-axis options
     :param options_y: All possible x-axis options
-    :return: The choosen x-axis and y-axis
+    :param options_char: All possible characteristic options
+    :return: The choosen x-axis and y-axis and characteristic
     """
-    if len(options_y) <= 0 or (len(options_x) <= 0):
-        return None, None
-    return options_x[0]['value'], options_y[0]['value']
+    if len(options_y) <= 0 or (len(options_x) <= 0) or (len(options_char) <= 0):
+        return None, None, None
+    return options_x[0]['value'], options_y[0]['value'], options_char[0]['value']
+
 
 @app.callback(Output('Mygraph', 'figure'), [
-
-Input('select-variable-x', 'value'),
-Input('select-variable-y', 'value'),
+    Input('upload-data', 'contents'),
+    Input('upload-data', 'filename'),
+    Input('select-variable-x', 'value'),
+    Input('select-variable-y', 'value'),
+    Input('select-characteristics', 'value'),
+    Input('select-plot-options', 'value')
 ])
-def update_graph(xvalue, yvalue):
+def update_graph(contents, filename, xvalue, yvalue, charvalue, plotvalue):
     """
     Displays the graph. Only normal plotting at the moment (x-axis, y-axis).
     TODO: Make different graphic plots possible.
@@ -185,46 +166,45 @@ def update_graph(xvalue, yvalue):
     :param filename: filename of the data
     :param xvalue: Value of the x-axis
     :param yvalue: value of the y-axis
+    :param charvalue: value of characteristic
+    :param plotvalue: Value of the plot selection e.g. scatter
     :return:  graph
     """
-    if (xvalue is None or yvalue is None):
+    if xvalue is None or yvalue is None:
         return {}
-    if (xvalue == "index" or yvalue == "index"):
+    if xvalue == "index" or yvalue == "index" or charvalue == "index":
         return {}
 
     x = []
     y = []
 
-    # if contents:
-    #     contents = contents[0]
-    #     filename = filename[0]
-    global df
-    if (df is not None):
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+    df = parse_data(contents, filename)
+    if df is not None:
 
-        # df = df.reset_index()
+        df = df.reset_index()
         x = df['{}'.format(xvalue)]
         y = df['{}'.format(yvalue)]
+        fig = go.Figure()
+        if('scatter' in plotvalue):
+            fig = px.scatter(df, x=x, y=y, color=charvalue, hover_data=df)
 
-        fig = go.Figure(
-            data=[
-                go.Scatter(
-                    x=x,
-                    y=y,
-                    mode='markers')
-                ],
-            layout=go.Layout(
-                plot_bgcolor=colors["graphBackground"],
-                paper_bgcolor=colors["graphBackground"]
-            ))
+        if('density' in plotvalue):
+            fig.add_histogram2d(x=x, y=y, colorscale='Blues', name='density', showlegend=False, showscale=False)
+
         return fig
     else:
         return {}
 
+
 @app.callback(Output('output-data-upload', 'children'),
-            [
-               Input('dummy', 'children')
-            ])
-def update_table(dummy):
+              [
+                  Input('upload-data', 'contents'),
+                  Input('upload-data', 'filename')
+              ])
+def update_table(contents, filename):
     """
     Makes a table from the uploaded data.
     :param contents: contents of the data
@@ -233,11 +213,11 @@ def update_table(dummy):
     """
     table = html.Div()
 
-    # if contents:
-    #     contents = contents[0]
-    #     filename = filename[0]
-    if dummy:
-        global df
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+        df = parse_data(contents, filename)
+
         table = html.Div([
             html.H5(filename),
             dash_table.DataTable(
@@ -249,12 +229,12 @@ def update_table(dummy):
 
             html.Pre(contents[0:200] + '...', style={
                 'whiteSpace': 'pre-wrap',
-                'wordBreak': 'break-all',
-                'display': 'none'
+                'wordBreak': 'break-all'
             })
         ])
 
     return table
+
 
 def parse_data(contents, filename):
     """
@@ -264,7 +244,7 @@ def parse_data(contents, filename):
     :param filename: filename of the data
     :return: Dataframe
     """
-    if (contents is None):
+    if contents is None:
         return
 
     content_type, content_string = contents.split(',')
@@ -273,7 +253,6 @@ def parse_data(contents, filename):
     try:
         if 'csv' in filename:
             # Assume that the user uploaded a CSV or TXT file
-            global df
             df = pd.read_csv(
                 io.StringIO(decoded.decode('utf-8')))
         elif 'xls' in filename:
@@ -282,13 +261,14 @@ def parse_data(contents, filename):
         elif 'txt' or 'tsv' in filename:
             # Assume that the user upl, delimiter = r'\s+'oaded an excel file
             df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')), delimiter = r'\s+')
+                io.StringIO(decoded.decode('utf-8')), delimiter=r'\s+')
     except Exception as e:
         print(e)
         return html.Div([
             'There was an error processing this file.'
         ])
     return df
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
