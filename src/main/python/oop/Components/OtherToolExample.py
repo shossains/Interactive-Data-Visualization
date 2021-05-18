ecdbe6267ac5402661fcdb9ca1488a16c3ea3606
joostjansenjan <j.j.jansen-2@student.tudelft.nl>
2021-05-18 13:44:41 +0200

    cleaned some code and added new time series example data

--- src/main/python/oop/Components/OtherToolExample.py
+++ src/main/python/oop/Components/OtherToolExample.py
@@ -8,7 +8,6 @@ from dash_oop_components import DashFigureFactory, DashComponent, DashComponentT
 from src.main.python.oop.Figure_factories import FigureFactories
 
 
-
 class ExampleML2(DashComponent):
     def __init__(self, plot_factory, df, title="Example ML"):
         """
@@ -25,37 +24,12 @@ class ExampleML2(DashComponent):
         page = dbc.Container([
             html.Div([
                 html.H3("This is another tool "),
-                ],
+            ],
                 id='OtherToolExample', style={'display': 'block'}),
         ], fluid=True)
         return page
 
     # def component_callbacks(self, app):
-    #     @app.callback(Output('Mygraph-ml2', 'figure'), [
-    #         Input('select-variable-x-ml2', 'value'),
-    #         Input('select-variable-y-ml2', 'value'),
-    #         Input('select-characteristics-ml2', 'value'),
-    #         Input('select-plot-options-ml2', 'value'),
-    #     ])
-    #     def update_plot(xvalue, yvalue, charvalue, plotvalue):
-    #         return self.plotfactory.graph_methods(self.df, xvalue, yvalue, charvalue, plotvalue)
-    #
-    #     @app.callback([Output('select-variable-x-ml2', 'options'),
-    #                    Output('select-variable-y-ml2', 'options'),
-    #                    Output('select-characteristics-ml2', 'options')],
-    #                   [
-    #                       Input('dummy', 'children')
-    #                   ])
-    #     def set_options_variable(dummy):
-    #         """
-    #         loads in possible parameters for the x and y-axis from the data.
-    #         :param dummy: dummy html property
-    #         :return: Possible options for dropdown x-axis.
-    #         """
-    #         dataframe = self.df.reset_index()
-    #         return [{'label': i, 'value': i} for i in dataframe.columns], [{'label': i, 'value': i} for i in
-    #                                                                        dataframe.columns], [
-    #                    {'label': i, 'value': i} for i in dataframe.columns]
 
     def set_data(self, data):
-            self.df = data
+        self.df = data
