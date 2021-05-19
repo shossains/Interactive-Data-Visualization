07b07a233fbc7c448a1db6efa9cfe10d20813e52
glennvdb <glenn-1999@hotmail.com>
2021-05-19 15:39:17 +0200

    Fixed small bug caused by a naming convention used by  dash testing api

--- dash_test/test_application.py
+++ dash_test/test_application.py
@@ -1,5 +1,6 @@
 from dash.testing.application_runners import import_app
 
+#Name of test method always has to start with test_ e.g. test_clickbutton or test_three
 
 def test_one(dash_duo):
     app = import_app("dash_test.app")
@@ -13,3 +14,11 @@ def test_two(dash_duo):
 
 
 #run using python -m pytest dash_test
+
+# from dash.testing.application_runners import import_app
+#
+#
+# def test_one(dash_duo):
+#     app = import_app("dash_test.app")
+#     dash_duo.start_server(app)
+#     dash_duo.wait_for_text_to_equal("h1", "Hello Dash", timeout=4)
