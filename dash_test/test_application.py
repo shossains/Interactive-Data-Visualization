49eb9bea62f0a3601e95f596fc28ce3a7df37b1c
glennvdb <glenn-1999@hotmail.com>
2021-06-14 17:30:16 +0200

    Added a few simple tests

--- dash_test/test_application.py
+++ dash_test/test_application.py
@@ -6,7 +6,18 @@ Run this test suite using: python -m pytest dash_test
 Find set of browser testing APIs and explanation at https://dash.plotly.com/testing
 '''
 
-def test_example(dash_duo):
-    app = import_app("src.main.python.oop.DashboardMain")
+def test_home_page_loaded(dash_duo):
+    app = import_app("src.main.python.oop.Main")
     dash_duo.start_server(app)
-    dash_duo.wait_for_text_to_equal("h1", "Interactive data visualizer", timeout=6)
+    dash_duo.wait_for_text_to_equal("#page-content", "This is the content of the home page!", timeout=5)
+
+def test_title_appearance(dash_duo):
+    app = import_app("src.main.python.oop.Main")
+    dash_duo.start_server(app)
+    dash_duo.wait_for_text_to_equal("#titleLink", "Interactive data visualizer", timeout=5)
+
+def test_title_appearance(dash_duo):
+    app = import_app("src.main.python.oop.Main")
+    dash_duo.start_server(app)
+    dash_duo.wait_for_text_to_equal("#titleLink", "Interactive data visualizer", timeout=5)
+
